aa3dcc3d5265797f9d40f40f81901c3d8ac1e264
Warsame <Warsame.mxi@hotmail.com>
2023-11-02 17:49:32 +0000

    Created Nat-gateway-module

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,11 @@
+# Nat-gateway variables
+variable "project_name" {}
+variable "enviroment" {}
+variable "public_subnet_az1_id" {}
+variable "internet_gateway" {}
+variable "public_subnet_az2_id" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az2_id" {}
