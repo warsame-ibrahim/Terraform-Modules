b6b07d71aec63131099937ad5a64043d5fd03091
Warsame <Warsame.mxi@hotmail.com>
2023-11-06 08:44:50 +0000

    Created ALB Module

---
+++ Alb.tf/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {}
+variable "enviroment" {}
+variable "alb_security_group_id" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "vpc_id" {}
+variable "certificate_arn" {}
+variable "target_type" {}
+
