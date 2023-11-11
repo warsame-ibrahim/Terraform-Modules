e8f59880530a6e2a311e521f1696eaa5ba7f5992
Warsame <Warsame.mxi@hotmail.com>
2023-11-11 19:23:14 +0000

    deleted ACM

--- acm/outputs.tf
+++
@@ -1,9 +0,0 @@
-# export the acm certificate arn
-output "certificate_arn" {
-  value = aws_acm_certificate.acm_certificate.arn
-}
-
-# export the domain name
-output "domain_name" {
-  value = var.domain_name
-}
